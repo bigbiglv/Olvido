ae393eb59853bd63fe7b8e9ecd62b26fd175c6c8
barry <barry.lin@gdautotoll.com.cn>
2026-06-12 14:10:38 +0800

    feat: 按钮组件使用新的大小

--- src/components/ui/button/variants.ts
+++ src/components/ui/button/variants.ts
@@ -14,10 +14,10 @@ export const buttonVariants = cva(
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-9 px-4 py-2',
-        sm: 'h-8 rounded-md px-3 text-xs',
-        lg: 'h-10 rounded-md px-8',
-        icon: 'size-9',
+        lg: 'h-8 px-3 py-2',
+        default: 'h-7 px-2 text-xs',
+        sm: 'h-6 px-1 text-xs',
+        icon: 'size-8',
       },
     },
     defaultVariants: {
